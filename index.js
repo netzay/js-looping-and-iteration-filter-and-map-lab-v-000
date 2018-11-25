1b163d4dc89abd621d1d0e7907a34c56c4e1fb02
Ally Netzel <netzelally@gmail.com>
2018-11-25 15:03:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-function driversWithRevenueOver (drivers, revenue) {
+function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) {
     return driver.revenue > revenue;
   });
 }
 
-function driverNamesWithRevenueOver (drivers, revenue) {
+function driverNamesWithRevenueOver(drivers, revenue) {
   return driversWithRevenueOver(drivers, revenue)
     .map(function (driver) {
       return driver.name;
