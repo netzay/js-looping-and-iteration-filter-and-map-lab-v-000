112336fddf812484cf8e3c54b2af2e56c11cef20
Ally Netzel <netzelally@gmail.com>
2018-11-25 15:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// Code your solution here:
-function driversWithRevenueOver(driver, revenue){
-   return driver.filter(function(driver){
-    return driver['revenue'] > revenue;
-  }); 
+
+function driversWithRevenueOver (drivers, revenue) {
+  return drivers.filter(function (driver) {
+    return driver.revenue > revenue;
+  });
 }
 
-function driverNamesWithRevenueOver(driver, revenue){
-  return driver.filter(function(driver){
-    if (driver['revenue'] > revenue) 
+function driverNamesWithRevenueOver (drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue)
+    .map(function (driver) {
       return driver.name;
-    }
-  )}
-  
+    });
 }
